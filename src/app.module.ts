6617cd8efa7cc7109a1236f222574a1261c7efab
Kwangyeon.Cho <rhkdwh91@gmail.com>
2024-10-29 22:28:17 +0900

    Refactor database config to use external file
    Moved database configuration into a separate typeORM config file to improve modularity and maintainability. This change centralizes database settings and cleans up the app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import * as dotenv from 'dotenv';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(typeORMConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,11 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+}
